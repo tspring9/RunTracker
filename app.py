d9a83b40a8783c53d11faab7af0ae890bac71bc5
tspring9 <73077185+tspring9@users.noreply.github.com>
2026-05-12 15:08:26 -0700

    Adjust colorbar position in app.py

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ with map_page:
         coloraxis_colorbar=dict(
             title="",
             orientation="h",
-            x=-0.5,
+            x=1,
             xanchor="center",
             y=-0.03,
             len=0.65,
